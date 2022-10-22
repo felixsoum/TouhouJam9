919d95d3b5fbee4486d62584205d0e42056b1dc9
Felix <felix@felixsoum.com>
2022-10-22 00:15:20 -0400

    Add picked mechanic

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Character : MonoBehaviour
     Material material;
 
     Camera mainCamera;
+    private bool isPicked;
 
     private void Awake()
     {
@@ -23,5 +25,17 @@ public class Character : MonoBehaviour
     private void Update()
     {
         meshRenderer.transform.forward = mainCamera.transform.forward;
+
+        if (isPicked && Input.GetMouseButtonUp(0))
+        {
+            isPicked = false;
+            material.SetFloat("_IsPicked", 0);
+        }
+    }
+
+    internal void MouseDown()
+    {
+        isPicked = true;
+        material.SetFloat("_IsPicked", 1);
     }
 }
